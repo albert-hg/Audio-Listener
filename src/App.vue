b330583c6ffdf8a82a32bd1c840ab6363fa467f1
albertHuang <yuda.h.ncku@gmail.com>
2020-12-09 18:53:21 +0800

    - Code refactoring.

--- src/App.vue
+++ src/App.vue
@@ -3,40 +3,40 @@
     <div>
       <input type="file" @change="browseAudioFile($event.target.files)" />
       <input type="text" v-model="audioUrl" />
-      <button type="button" @click="loadResource(audioUrl)">載入網址資源</button>
+      <button type="button" @click="loadResourceInWavesurfer(audioUrl)">載入網址資源</button>
     </div>
-    <div id="waveform-container" style="width: 100%; overflow: auto;">
-      <div id="waveform" ref="waveform" style="width: fit-content;"></div>
+    <div id="js__waveform-container" style="width: 100%; overflow: auto;">
+      <div id="js__waveform" ref="waveform" style="width: fit-content;"></div>
     </div>
-    <div id="waveform-controller" v-if="wavesurfer && wavesurfer.backend.buffer">
-      <button type="button" @click="switchPlayPause()">
-        <span v-if="isNowPlaying">暫停</span>
+    <div id="js__waveform-controller" v-if="wavesurfer && wavesurfer.backend.buffer">
+      <button type="button" @click="switchPlayAndPause()">
+        <span v-if="playingController.isAudioPlayingNow">暫停</span>
         <span v-else>播放</span>
       </button>
-      <button type="button" @click="switchReplayMode()">
-        <span v-if="isReplayMode">關閉循環撥放</span>
-        <span v-else>開起循環撥放</span>
-      </button>
-      <button type="button" @click="switchKeepUpdatePosition()">
-        <span v-if="isKeepUpdatePosition">取消定位</span>
+      <div>
+        <input type="checkbox" id="replayMode" v-model="playingController.isReplayMode" />
+        <label for="replayMode">循環撥放</label>
+      </div>
+      <button type="button" @click="switchPositionCenterAlways()">
+        <span v-if="playingController.isPositionCenterAlways">取消定位</span>
         <span v-else>啟動定位</span>
       </button>
       <button @click="analyzeAduioParagraph()">分析斷句</button>
       <div>
         <span>播放速度</span>
-        <input type="range" min="5" max="15" value="10" @change="setPlayRate($event.target.value)" />
+        <input type="range" min="5" max="15" v-model="playingController.playingSpeedRate" @change="setPlayRate()" />
       </div>
       <div>
         <span>說話峰值(建議為8~15)</span>
-        <input type="number" v-model="audioMinTalkingPeakThreshold" />
+        <input type="number" min="1" v-model="analyzationController.thresholdForMinSpeakingPeakSize" />
       </div>
       <div>
         <span>允許停頓毫秒數(建議為500毫秒)</span>
-        <input type="number" v-model="audioAllowTalkingPauseMillisecond" />
+        <input type="number" min="1" v-model="analyzationController.allowedPauseMillisecondsInSpeaking" />
       </div>
       <div>
         <span>播放緩衝毫秒數(建議為50毫秒)</span>
-        <input type="number" v-model="audioBufferTimeAsMillisecond" />
+        <input type="number" min="1" v-model="playingController.playingBufferMillisecond" />
       </div>
     </div>
     <div id="split-data-container" v-if="wavesurfer && wavesurfer.backend.buffer">
@@ -49,12 +49,12 @@
           </tr>
         </thead>
         <tbody>
-          <tr v-for="re in analyzeAduioParagraphResult" :key="re.index">
+          <tr v-for="re in audioInfos.paragraghResult" :key="re.index">
             <td>{{re.startTime}}</td>
             <td>{{re.endTime}}</td>
             <td>{{re.during}}</td>
             <td>
-              <button type="button" @click="playFromTimeToTime(re.startTime, re.endTime)">播放</button>
+              <button type="button" @click="playParagraph(re)">播放</button>
             </td>
           </tr>
         </tbody>
@@ -72,45 +72,79 @@ export default {
   data() {
     return {
       wavesurfer: null,
-      peakSizePerSec: 40,
-      audioPeakData: [],
       audioUrl:
         "https://stitcher.acast.com/livestitches/MTFBRjRFMUUtNjhCRi00MEFFLUJCQTRBMEMxQjNBNUY1Q0I=/f4d291fbf2aee915622fa5ce03f3b67d.mp3?ci=NZ32SCWYv2Zzy-sQo0jQlJjELe2yS_XABLwfqSvhzzgYCKnZUA2S6Q%3D%3D&pf=rss&range=bytes%3D0-&sv=sphinx%401.42.2&uid=c18ecc397c9a658b6b0fdaf1297d999e&Expires=1620033876&Signature=NGXOldiJfosLXcbkIFDDjWO%7EUFteynpvoagVOVj9X0v2ch3xPu8-Txyu-F%7EXApGgXw5s7fpt5zas1kTmsiB6FCXKhm87CG%7EHzUNqkV3ixNzU7Tm%7EGT-Abf%7EcHpX9V6aWAyWfatFY%7EICK3LyEFZZVOxXfzYcWLEomHsNYkQkgE94WzrJ9kxicjqyxDe16xNPioWTeUwatZAHhdF62MVUaVHKnT9qQPzFKxRYiQ6JESLz3EtusTt2ErOMedwHlZODYAzFpqzie%7ELJoVfqzsOMGBisCZIuqbzainmtvRnkPYMBKRexeKtbzzflI58Ensaex-g97pcyywEeA6T%7E8Fcs7bA__&Key-Pair-Id=APKAJXAFARUOTJQ3BLOQ",
-      isKeepUpdatePosition: true,
-      isNowPlaying: false,
-      isReplayMode: false,
-      analyzeAduioParagraphResult: [],
-      audioMinTalkingPeakThreshold: 10,
-      audioAllowTalkingPauseMillisecond: 500,
-      audioBufferTimeAsMillisecond: 50,
-      audioPlayFromTime: 0,
-      audioPlayToTime: 0,
+      audioInfos: {
+        totalDurationTime: 0,
+        peakNumberPerSecond: 40,
+        originalPeakData: [],
+        paragraghResult: [],
+      },
+      analyzationController: {
+        thresholdForMinSpeakingPeakSize: 10,
+        allowedPauseMillisecondsInSpeaking: 500,
+      },
+      playingController: {
+        fromTime: 0,
+        toTime: 0,
+        isPositionCenterAlways: true,
+        isAudioPlayingNow: false,
+        isReplayMode: false,
+        playingBufferMillisecond: 50,
+        playingSpeedRate: 10,
+      },
     };
   },
   mounted() {
     this.initCreateWaveSurfer();
   },
   beforeDestroy() {
-    this.clearAllInterval();
+    this.unregisterPositionCenter();
+    this.wavesurfer.destroy();
   },
   methods: {
     initCreateWaveSurfer() {
       this.wavesurfer = WaveSurfer.create({
-        container: "#waveform",
+        container: "#js__waveform",
         waveColor: "#2682d4",
         progressColor: "#2682d4",
         backend: "MediaElement",
         fillParent: false,
-        minPxPerSec: this.peakSizePerSec,
+        minPxPerSec: this.audioInfos.peakNumberPerSecond,
       });
     },
-    loadResource(res) {
-      clearTimeout(this.$options.keepUpdatingTogglePosition);
+    loadResourceInWavesurfer(res) {
+      this.unregisterPositionCenter();
       this.wavesurfer.unAll();
-      this.wavesurfer.load(res);
       this.setWavesurferOnWaveformReady();
       this.setWavesurferOnPause();
-      this.setWavesurferOnFinish();
+      this.wavesurfer.load(res);
+    },
+    setWavesurferOnWaveformReady() {
+      this.wavesurfer.on("waveform-ready", () => {
+        console.debug("The Time of Audio (second): " + this.wavesurfer.backend.buffer.duration);
+        let data = this.wavesurfer.backend.mergedPeaks.map((d) => {
+          return !d ? 0 : Number.parseInt(d * 1000);
+        });
+        console.debug(data.length);
+        this.audioInfos.originalPeakData = data;
+        this.audioInfos.totalDurationTime = this.wavesurfer.getDuration();
+        this.playingController.toTime = this.wavesurfer.getDuration();
+        this.analyzeAduioParagraph();
+      });
+    },
+    setWavesurferOnPause() {
+      this.wavesurfer.on("pause", () => {
+        this.unregisterPositionCenter();
+        if (this.playingController.isReplayMode && this.playingController.isAudioPlayingNow) {
+          this.doPlay();
+        } else {
+          this.playingController.isAudioPlayingNow = false;
+        }
+      });
+    },
+    setPlayRate() {
+      this.wavesurfer.setPlaybackRate(this.playingController.playingSpeedRate / 10);
     },
     browseAudioFile(files = []) {
       if (!!files || files.length == 1) {
@@ -120,7 +154,7 @@ export default {
           let blob = new window.Blob([new Uint8Array(evt.target.result)]);
           let audio = new Audio();
           audio.src = URL.createObjectURL(blob);
-          this.loadResource(audio);
+          this.loadResourceInWavesurfer(audio);
         };
         reader.onerror = (evt) => {
           console.error("Error, ", evt);
@@ -128,93 +162,33 @@ export default {
         reader.readAsArrayBuffer(file);
       }
     },
-    setPlayRate(evt) {
-      this.wavesurfer.setPlaybackRate(evt / 10);
-    },
-    setWavesurferOnWaveformReady() {
-      let wavesurfer = this.wavesurfer;
-      wavesurfer.on("waveform-ready", () => {
-        console.debug("The Time of Audio (second): " + wavesurfer.backend.buffer.duration);
-        let data = wavesurfer.backend.mergedPeaks.map((d) => {
-          return !d ? 0 : Number.parseInt(d * 1000);
-        });
-        console.debug(data.length);
-        this.audioPeakData = data;
-        this.wavesurfer = wavesurfer;
-        this.analyzeAduioParagraph();
-      });
-    },
-    setWavesurferOnPause() {
-      this.wavesurfer.on("pause", () => {
-        console.debug("pause");
-        this.isNowPlaying = false;
-        this.clearAllInterval();
-        if (this.isReplayMode) {
-          this.playFromTimeToTime(this.audioPlayFromTime, this.audioPlayToTime);
-        }
-      });
-    },
-    setWavesurferOnFinish() {
-      this.wavesurfer.on("finish", () => {
-        // Cancel this field code. This is due to the event 'pause' will be triggered before on finish event.
-        // If you really want to do something on finished, add some procedure here.
-      });
-    },
-    setKeepUpdatePositionInterval() {
-      if (!this.$options.keepUpdatingTogglePosition) {
-        this.$options.keepUpdatingTogglePosition = setInterval(() => {
-          this.toogleToNowPosition();
-        }, 10);
-      }
-    },
-    toogleToNowPosition() {
-      let audioTotalTime = this.wavesurfer.backend.buffer.duration;
-      let waveformTotalWidth = document.getElementById("waveform").offsetWidth;
-      let containerWidth = document.getElementById("waveform-container").offsetWidth;
-      let waveformContainer = document.getElementById("waveform-container");
-      let nowScrollPosition =
-        (this.wavesurfer.getCurrentTime() * waveformTotalWidth) / audioTotalTime - containerWidth / 2;
-      waveformContainer.scrollTo(nowScrollPosition, 0);
-      console.debug(nowScrollPosition);
-    },
-    clearAllInterval() {
-      clearInterval(this.$options.keepUpdatingTogglePosition);
-      this.$options.keepUpdatingTogglePosition = null;
-    },
-    switchKeepUpdatePosition() {
-      if (this.isKeepUpdatePosition) {
-        this.clearAllInterval();
-      } else if (!this.isKeepUpdatePosition && this.wavesurfer.isPlaying()) {
-        this.setKeepUpdatePositionInterval();
-      }
-      this.isKeepUpdatePosition = !this.isKeepUpdatePosition;
-    },
-    switchPlayPause() {
-      if (!this.isNowPlaying && this.isKeepUpdatePosition) {
-        this.audioPlayFromTime = 0;
-        this.audioPlayToTime = this.wavesurfer.getDuration();
-        this.setKeepUpdatePositionInterval();
-      }
-      this.wavesurfer.playPause();
-      this.isNowPlaying = !this.isNowPlaying;
-    },
-    switchReplayMode() {
-      this.isReplayMode = !this.isReplayMode;
-    },
     analyzeAduioParagraph() {
-      let data = this.audioPeakData;
+      let result = this.analyzeAduio_mergeByAllowedSpeakingPauseTime(
+        this.analyzeAduio_splitDataByMinSpeakingPeakSize()
+      );
+      this.audioInfos.paragraghResult = result.map((d) => {
+        let st = this.parsePeakIndexToTimeAsMs(d.startPeakIndex);
+        let et = this.parsePeakIndexToTimeAsMs(d.endPeakIndex);
+        return {
+          startTime: st / 1000,
+          endTime: et / 1000,
+          during: (et - st) / 1000,
+        };
+      });
+    },
+    analyzeAduio_splitDataByMinSpeakingPeakSize() {
+      let data = this.audioInfos.originalPeakData;
       let startPoiter = 0;
-      let minTalkingPeakThreshold = this.audioMinTalkingPeakThreshold; // 視為說話的閥值
-      let temp = [];
-
+      let minTalkingPeakThreshold = this.analyzationController.thresholdForMinSpeakingPeakSize; // 視為說話的閥值
+      let result = [];
       for (let endPointer = 0; endPointer < data.length; endPointer++) {
         let dataAbs = Math.abs(data[endPointer]);
         if (dataAbs < minTalkingPeakThreshold) {
           if (startPoiter < endPointer) {
             // stop talking
-            temp.push({
-              sp: startPoiter,
-              ep: endPointer,
+            result.push({
+              startPeakIndex: startPoiter,
+              endPeakIndex: endPointer,
             });
             startPoiter = endPointer + 1;
           } else if (startPoiter >= endPointer) {
@@ -223,40 +197,93 @@ export default {
           }
         }
       }
-
-      let allowTalkingPausePeadSize = (this.audioAllowTalkingPauseMillisecond * this.peakSizePerSec) / 1000; // 說話停頓許可Peak數
+      return result;
+    },
+    analyzeAduio_mergeByAllowedSpeakingPauseTime(arr_sdbmsp) {
+      let allowTalkingPausePeadSize =
+        (this.analyzationController.allowedPauseMillisecondsInSpeaking * this.audioInfos.peakNumberPerSecond) / 1000; // 說話停頓許可Peak數
       let result = [];
-      for (let endPointer = 0; endPointer < temp.length; endPointer++) {
+      for (let endPointer = 0; endPointer < arr_sdbmsp.length; endPointer++) {
         if (endPointer == 0) {
-          result.push(temp[0]);
-        } else if (temp[endPointer].sp - result[result.length - 1].ep < allowTalkingPausePeadSize) {
-          result[result.length - 1].ep = temp[endPointer].ep;
+          result.push(arr_sdbmsp[0]);
+        } else if (
+          arr_sdbmsp[endPointer].startPeakIndex - result[result.length - 1].endPeakIndex <
+          allowTalkingPausePeadSize
+        ) {
+          result[result.length - 1].endPeakIndex = arr_sdbmsp[endPointer].endPeakIndex;
         } else {
-          result.push(temp[endPointer]);
+          result.push(arr_sdbmsp[endPointer]);
         }
       }
-      this.analyzeAduioParagraphResult = result.map((d) => {
-        let st = this.parsePeaKIndexToTimeAsMs(d.sp);
-        let et = this.parsePeaKIndexToTimeAsMs(d.ep);
-        return {
-          startTime: st / 1000,
-          endTime: et / 1000,
-          during: (et - st) / 1000,
-        };
-      });
+      return result;
     },
-    playFromTimeToTime(startTime, endTime) {
-      if (!this.isNowPlaying && this.isKeepUpdatePosition) {
-        this.setKeepUpdatePositionInterval();
+    parsePeakIndexToTimeAsMs(peakIndex) {
+      return Math.round(
+        (peakIndex * this.audioInfos.totalDurationTime * 1000) / this.audioInfos.originalPeakData.length
+      );
+    },
+    switchPlayAndPause() {
+      if (!this.playingController.isAudioPlayingNow) {
+        this.doPlay();
+      } else {
+        this.doPause();
       }
-      this.audioPlayFromTime = startTime;
-      this.audioPlayToTime = endTime;
-      let bufferTime = this.audioBufferTimeAsMillisecond / 1000;
-      this.wavesurfer.play(startTime - bufferTime, endTime + bufferTime);
-      this.isNowPlaying = true;
     },
-    parsePeaKIndexToTimeAsMs(peakIndex) {
-      return Math.round((peakIndex * this.wavesurfer.backend.buffer.duration * 1000) / this.audioPeakData.length);
+    doPlay() {
+      if (this.playingController.isPositionCenterAlways) {
+        this.registerPositionCenter();
+      }
+      this.playingController.isAudioPlayingNow = true;
+      let bufferTime = this.playingController.playingBufferMillisecond / 1000;
+      let startTime = this.playingController.fromTime - bufferTime;
+      let endTime = this.playingController.toTime + bufferTime;
+      let currentTime = this.wavesurfer.getCurrentTime();
+      if (currentTime >= this.playingController.fromTime && currentTime <= this.playingController.toTime) {
+        startTime = currentTime;
+      }
+      this.wavesurfer.play(startTime, endTime);
+    },
+    doPause() {
+      this.unregisterPositionCenter();
+      this.playingController.isAudioPlayingNow = false;
+      // this.playingController.isReplayMode = false;
+      this.wavesurfer.pause();
+    },
+    registerPositionCenter() {
+      if (!this.$options.intervel_positionCenter) {
+        this.$options.intervel_positionCenter = setInterval(() => {
+          this.toggleNowPositionToCenter();
+        }, 10);
+      }
+    },
+    playParagraph(paragragh) {
+      this.playingController.fromTime = paragragh.startTime;
+      this.playingController.toTime = paragragh.endTime;
+      this.doPlay();
+    },
+    unregisterPositionCenter() {
+      clearInterval(this.$options.intervel_positionCenter);
+      this.$options.intervel_positionCenter = null;
+    },
+    toggleNowPositionToCenter() {
+      let audioTotalTime = this.audioInfos.totalDurationTime;
+      let waveformTotalWidth = document.getElementById("js__waveform").offsetWidth;
+      let containerWidth = document.getElementById("js__waveform-container").offsetWidth;
+      let waveformContainer = document.getElementById("js__waveform-container");
+      let nowScrollPosition =
+        (this.wavesurfer.getCurrentTime() * waveformTotalWidth) / audioTotalTime - containerWidth / 2;
+      waveformContainer.scrollTo(nowScrollPosition, 0);
+    },
+    switchPositionCenterAlways() {
+      if (!this.playingController.isPositionCenterAlways) {
+        if (this.playingController.isAudioPlayingNow) {
+          this.registerPositionCenter();
+        }
+        this.playingController.isPositionCenterAlways = true;
+      } else {
+        this.unregisterPositionCenter();
+        this.playingController.isPositionCenterAlways = false;
+      }
     },
   },
 };
